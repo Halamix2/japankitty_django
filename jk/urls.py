c9af40436933dce8bfea9cfec90d6d1bc6e939dc
Piotr Halama <skrzynka@halamix2.pl>
2020-02-10 22:33:13 +0100

    add course controller

---
+++ jk/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, re_path, include
+from django.contrib import admin
+from django.http import JsonResponse
+
+from . import views
+
+urlpatterns = [
+    path('api/', include([
+        path('v1/', include(
+            [
+            path('courses/', views.courses),
+            ])
+        ),
+    ])),
+]
--- jk/views.py
+++ jk/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
+from .models import Course
 # Create your views here.
+
+def courses(request):
+    data = list(Course.objects.values())
+
+    #allow returning non-dict data, an array in our case
+    return JsonResponse(data, safe=False)
