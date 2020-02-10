aa24b6cb6331d0d588c53d73611e7ad605fc5945
Piotr Halama <skrzynka@halamix2.pl>
2020-02-10 22:23:15 +0100

    add jk module
    Most of the application logic will be in that module

--- japankitty/settings.py
+++ japankitty/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jk.apps.JkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
